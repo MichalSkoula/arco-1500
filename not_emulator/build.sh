14308aefe24812b6736318002410c01f3fa27d2d
David Donátek <david.donatek@gmail.com>
2019-04-12 00:35:41 +0200

    not_emulator: fixed build script first looks into ../arduino for portable version of arduino distribution then it tries /usr/share/arduino

--- not_emulator/build.sh
+++ not_emulator/build.sh
@@ -11,6 +11,7 @@ fi
 
 SKETCH="../games/$1/$1.ino"
 BUILD="build"
+LIBS="../arduino/portable/sketchbook/libraries"
 PREP="$BUILD/preprocess"
 
 if [ ! -f "$SKETCH" ]; then
@@ -26,14 +27,39 @@ mkdir "$PREP"
 
 # TODO get rid of arduino-builder dependency - concat .ino files and "just" add forward declarations
 # TODO ctags? nope just replacing dependency with another one
-/usr/share/arduino/arduino-builder				\
-	-preprocess 								\
-	-hardware /usr/share/arduino/hardware 		\
-	-tools /usr/share/arduino/tools-builder		\
-	-libraries ~/Arduino/libraries				\
-	-fqbn=arduino:avr:nano:cpu=atmega328		\
-	-build-path "$PREP"							\
-	"$SKETCH"									> /dev/null
+# TODO OR just write all forward declarations by hand for every game?
+
+ARDUINO="../arduino"
+if [ -f "$ARDUINO/arduino-builder" ]; then
+    echo "Running arduino-builder (portable) in '$ARDUINO'"
+
+    "$ARDUINO/arduino-builder"                      \
+        -preprocess 								\
+        -hardware "$ARDUINO/hardware"       		\
+        -tools "$ARDUINO/tools-builder"     		\
+        -tools "$ARDUINO/hardware/tools/avr"   		\
+        -libraries "$LIBS"                  		\
+        -fqbn=arduino:avr:nano:cpu=atmega328		\
+        -build-path "$PREP"							\
+        "$SKETCH"									> /dev/null
+else
+    ARDUINO="/usr/share/arduino"
+    echo "Running arduino-builder (system) in '$ARDUINO'"
+
+    # copy gamelib to default Arduino IDE Sketchbook location
+    # TODO read location from ~/.arduino15/preferences.txt sketchbook.path=?
+    mkdir -p "$HOME/Arduino/libraries/gamelib"
+    cp "$LIBS/gamelib/"* "$HOME/Arduino/libraries/gamelib"
+
+    "$ARDUINO/arduino-builder"                      \
+        -preprocess 								\
+        -hardware "$ARDUINO/hardware"       		\
+        -tools "$ARDUINO/tools-builder"     		\
+        -libraries "$HOME/Arduino/libraries"    	\
+        -fqbn=arduino:avr:nano:cpu=atmega328		\
+        -build-path "$PREP"							\
+        "$SKETCH"									> /dev/null
+fi
 
 cp "$PREP/sketch/$1.ino.cpp" "$BUILD/main.cpp"
 cat main.cpp >> "$BUILD/main.cpp"
@@ -41,11 +67,11 @@ cat main.cpp >> "$BUILD/main.cpp"
 # TODO cmake
 # TODO -Wconversion
 cd "$BUILD"
-g++ -std=c++17                                                                  \
-    -Wall -Wpedantic -pedantic-errors -Wextra                                   \
-    -I ".." -I "../../arduino/portable/sketchbook/libraries/gamelib"            \
-    -D "WINDOW_TITLE=\"not_emulator - $1\""                                     \
-	-o "$1"																		\
-    main.cpp ../Arduino.cpp ../EEPROM.cpp ../U8g2lib.cpp                    	\
-    ../../arduino/portable/sketchbook/libraries/gamelib/*.cpp                   \
+g++ -std=c++17                                                  \
+    -Wall -Wpedantic -pedantic-errors -Wextra                   \
+    -I ".." -I "../$LIBS/gamelib"                               \
+    -D "WINDOW_TITLE=\"not_emulator - $1\""                     \
+    -o "$1"                                                     \
+    main.cpp ../Arduino.cpp ../EEPROM.cpp ../U8g2lib.cpp        \
+    "../$LIBS/gamelib/"*.cpp                                    \
 	-lSDL2 -lSDL2_ttf

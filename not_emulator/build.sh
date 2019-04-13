b115bad1210cb84c7e56d6ab17850fae4d84e39a
David Donátek <david.donatek@gmail.com>
2019-04-13 21:07:47 +0200

    not_emulator: improved build.sh - fallback if arduino is not found
    fallback option searches .ino files, finds all structs classes and
    functions
    then concats all .ino files and adds forward declarations of the structs,
    classes and functions
    
    just a few lines of grep and sed, probably very fragile

--- not_emulator/build.sh
+++ not_emulator/build.sh
@@ -3,17 +3,20 @@
 # ./build.sh <game directory name>
 # e.g. ./build.sh galaxy_madness
 
+# TODO add parameter --no-arduino to use fallback option instead of arduino-builder
+
 if [ "$#" -ne 1 ]; then
 	echo "Must be called with exactly one parameter - name of the game"
 	echo "See the games directory"
 	exit
 fi
 
-SKETCH="../games/$1/$1.ino"
+GAME="../games/$1"
 BUILD="build"
+MAIN="main.cpp"
 LIBS="../arduino/portable/sketchbook/libraries"
-PREP="$BUILD/preprocess"
 
+SKETCH="$GAME/$1.ino"
 if [ ! -f "$SKETCH" ]; then
 	echo "Sketch with path '$SKETCH' does not exist"
 	exit
@@ -23,55 +26,90 @@ if [ -d "$BUILD" ]; then
 	rm -r "$BUILD"
 fi
 mkdir "$BUILD"
-mkdir "$PREP"
 
-# TODO get rid of arduino-builder dependency - concat .ino files and "just" add forward declarations
-# TODO ctags? nope just replacing dependency with another one
-# TODO OR just write all forward declarations by hand for every game?
+ARDUINO_PORT="../arduino"
+ARDUINO_SYS="/usr/share/arduino"
+# TODO [ ! no-arduino ] && { ... }
+if [ -f "$ARDUINO_PORT/arduino-builder" ] || [ -f "$ARDUINO_SYS/arduino-builder" ]; then
+    PREP="$BUILD/preprocess"
+    mkdir -p "$PREP"
 
-ARDUINO="../arduino"
-if [ -f "$ARDUINO/arduino-builder" ]; then
-    echo "Running arduino-builder (portable) in '$ARDUINO'"
+    ARDUINO="$ARDUINO_PORT"
+    if [ -f "$ARDUINO/arduino-builder" ]; then
+        echo "Running arduino-builder (portable) in '$ARDUINO'"
 
-    "$ARDUINO/arduino-builder"                      \
-        -preprocess 								\
-        -hardware "$ARDUINO/hardware"       		\
-        -tools "$ARDUINO/tools-builder"     		\
-        -tools "$ARDUINO/hardware/tools/avr"   		\
-        -libraries "$LIBS"                  		\
-        -fqbn=arduino:avr:nano:cpu=atmega328		\
-        -build-path "$PREP"							\
-        "$SKETCH"									> /dev/null
+        "$ARDUINO/arduino-builder"                      \
+            -preprocess 								\
+            -hardware "$ARDUINO/hardware"       		\
+            -tools "$ARDUINO/tools-builder"     		\
+            -tools "$ARDUINO/hardware/tools/avr"   		\
+            -libraries "$LIBS"                  		\
+            -fqbn=arduino:avr:nano:cpu=atmega328		\
+            -build-path "$PREP"							\
+            "$SKETCH"									> /dev/null
+    else
+        ARDUINO="$ARDUINO_SYS"
+        echo "Running arduino-builder (system) in '$ARDUINO'"
+
+        # copy gamelib to default Arduino IDE Sketchbook location
+        # TODO read location from ~/.arduino15/preferences.txt sketchbook.path=?
+        mkdir -p "$HOME/Arduino/libraries/gamelib"
+        cp "$LIBS/gamelib/"* "$HOME/Arduino/libraries/gamelib"
+
+        "$ARDUINO/arduino-builder"                      \
+            -preprocess 								\
+            -hardware "$ARDUINO/hardware"       		\
+            -tools "$ARDUINO/tools-builder"     		\
+            -libraries "$HOME/Arduino/libraries"    	\
+            -fqbn=arduino:avr:nano:cpu=atmega328		\
+            -build-path "$PREP"							\
+            "$SKETCH"									> /dev/null
+    fi
+
+    cp "$PREP/sketch/$1.ino.cpp" "$BUILD/main.cpp"
 else
-    ARDUINO="/usr/share/arduino"
-    echo "Running arduino-builder (system) in '$ARDUINO'"
+    echo "Concatenating game .ino files (no arduino)..."
+    # --------------------------------------------------
+    # concatenate all .ino files into one file main.cpp
+    # --------------------------------------------------
+    echo -e "#include <Arduino.h>" > "$BUILD/$MAIN"
 
-    # copy gamelib to default Arduino IDE Sketchbook location
-    # TODO read location from ~/.arduino15/preferences.txt sketchbook.path=?
-    mkdir -p "$HOME/Arduino/libraries/gamelib"
-    cp "$LIBS/gamelib/"* "$HOME/Arduino/libraries/gamelib"
+    # TODO probably lots of problems
+    # TODO multiline /**/ comments can cause problems
+    # TODO default values (meh, arduino can't deal with them either)
 
-    "$ARDUINO/arduino-builder"                      \
-        -preprocess 								\
-        -hardware "$ARDUINO/hardware"       		\
-        -tools "$ARDUINO/tools-builder"     		\
-        -libraries "$HOME/Arduino/libraries"    	\
-        -fqbn=arduino:avr:nano:cpu=atmega328		\
-        -build-path "$PREP"							\
-        "$SKETCH"									> /dev/null
+    # extract structs and classes from all .ino files and add as forward declarations
+    grep -oE --no-filename '^(struct|class).*?\s*\{?\s*$' "$GAME/"*.ino     | \
+        sed 's/{//g' | sed 's/$/;/g'                                        >> "$BUILD/$MAIN"
+
+    # extract function headers from all .ino files and add as forward declarations
+    #   match everything that resembles function header
+    #   filter out lines starting if/else/for/...
+    #   remove { and add semicolon ; at the end of every line
+    grep -oE --no-filename '^([^/\*\}])*\(.*?\)\s*\{?\s*$' "$GAME/"*.ino    | \
+        grep -E --invert-match '\s*(if|else|else if|switch|for|while|do)'   | \
+        sed 's/{//g' | sed 's/$/;/g'                                        >> "$BUILD/$MAIN"
+
+    cat "$SKETCH" >> "$BUILD/$MAIN"
+
+    for file in "$GAME/"*.ino; do
+        if [ "$file" != "$SKETCH" ]; then
+            cat "$file" >> "$BUILD/$MAIN"
+        fi
+    done
 fi
 
-cp "$PREP/sketch/$1.ino.cpp" "$BUILD/main.cpp"
-cat main.cpp >> "$BUILD/main.cpp"
+cat "$MAIN" >> "$BUILD/$MAIN"
 
 # TODO cmake
 # TODO -Wconversion
 cd "$BUILD"
+echo "Compiling..."
 g++ -std=c++17                                                  \
     -Wall -Wpedantic -pedantic-errors -Wextra                   \
     -I ".." -I "../$LIBS/gamelib"                               \
     -D "WINDOW_TITLE=\"not_emulator - $1\""                     \
     -o "$1"                                                     \
-    main.cpp ../Arduino.cpp ../EEPROM.cpp ../U8g2lib.cpp        \
+    "$MAIN" ../Arduino.cpp ../EEPROM.cpp ../U8g2lib.cpp         \
     "../$LIBS/gamelib/"*.cpp                                    \
 	-lSDL2 -lSDL2_ttf

56d32dc083b9ad524428ebadc69c861ece9db692
MichalSkoula <michal@exitshop.cz>
2019-08-20 17:49:07 +0200

    Flappy Cat - pipes

--- games/flappy_cat/flappy_cat.ino
+++ games/flappy_cat/flappy_cat.ino
@@ -12,12 +12,13 @@ static unsigned char playerBigBits[] = {
    0x06, 0x60, 0xfc, 0x3f, 0xf8, 0x1f, 0x00, 0x00 };
 
 /* classes ------------------------------------------------------------------- */
-struct Game {
+struct Game 
+{
     public:
         byte width = 128;
         byte height = 64;
-
-        byte speed = 2;
+        byte sidebarWidth = 32;
+        bool step = true;
 
         // 0 menu
         // 1 game
@@ -31,7 +32,7 @@ struct Game {
             display.drawXBM(104, 4, 16, 16, playerBigBits);
         }
 
-        void drawGame()
+        void draw()
         {
             display.drawVLine(96, 0, height);
             display.drawSmallText(98, 6, "SCORE");
@@ -39,22 +40,35 @@ struct Game {
         }
 } game;
 
-class Player {
-    public:
-        byte width = 8;
-        byte height = 8;
+class Player 
+{
+    public:     
+        byte getX()
+        {
+            return x;
+        }  
 
-        byte x = 10;
-        int y = 0;
+        byte getY()
+        {
+            return y;
+        }       
 
-        byte health = 100;
-                
+        byte getWidth()
+        {
+            return width;
+        }
+
+        byte getHeight()
+        {
+            return height;
+        }
+
         void draw()
         {
             // health
             display.drawSmallText(98, 26, "HEALTH");
-            display.drawFrame(98, 32, 24, 4);
-            display.drawBox(98, 32, health / 4.16, 4);
+            display.drawFrame(98, 32, 28, 4);
+            display.drawBox(98, 32, health / 3.57, 4);
 
             // player
             if (y > 0 || y < game.height - height) {
@@ -62,36 +76,140 @@ class Player {
             }
         }
 
-        void die()
+        void hurt(byte howMuch)
         {
-            game.stage = 2;
-            sound.playTone(100, 30);
+            if (health > howMuch) {
+                health -= howMuch;
+                sound.playTone(100);
+            } else {
+                game.stage = 2;
+                sound.playTone(100, 300);
+            }
         }
 
         void checkMovement()
         {
             if (buttonDown(ACTION_BUTTON)) {
-                y -= 4;
+                // jump
+                y -= 6;
                 sound.playTone(200);
             } else {
-                y += 2;
+                // fall down
+                y += 3;
             }
         }
 
-        void checkCollision()
+        void loop()
         {
-            // floor and ceiling
+            game.step = !game.step;
+
+            // collision - floor and ceiling
             if (y < 0 || y > game.height - height) {
-                if (health > 0) {
-                    health--;
-                } else {
-                    die();
-                }
+                hurt(2);
+            } else  if (game.step % 2 == 0) {
+                scoreTable.addScore(1);
+            }
+        }
+
+    private:
+        byte width = 8;
+        byte height = 8;
+
+        byte x = 10;
+        int y = 0;
+
+        byte health = 100;
+
+} player;
+
+class Pipe 
+{
+    public:
+        void setX(int newX)
+        {
+            x = newX;
+        }
+
+        void draw()
+        {
+            if (x > 0 && x < game.width - game.sidebarWidth) {
+                // up part
+                display.drawBox(x, 0, width, y - space / 2);
+                // down part
+                display.drawBox(x, y + space / 2, width, game.height);
+            }
+        } 
+
+        void move()
+        {
+            if (game.step % 2 == 0) {
+                x -= 3;
             }
 
-            // blocks ...
+            if (x - width < 0) {
+                x = random(game.width, game.width * 1.5);
+                y = random(space, game.height - space);
+                space = random(space / 1.5, space);
+            }
         }
-} player;
+
+        void checkCollisionWithPlayer()
+        {
+            if (
+                (
+                    player.getX() < x + width && player.getX() + player.getWidth() > x &&
+                    player.getY() < y - space / 2 && player.getY() + player.getHeight() > 0
+                )
+                ||
+                (
+                    player.getX() < x + width && player.getX() + player.getWidth() > x &&
+                    player.getY() < game.height && player.getY() + player.getHeight() > y + space / 2
+                )
+            ) {
+                
+                // up part
+                display.drawBox(x, 0, width, y - space / 2);
+                // down part
+                display.drawBox(x, y + space / 2, width, game.height);
+                player.hurt(3);
+            }
+        }
+
+    private: 
+        int x = game.width;
+        int y = game.height / 2;
+        byte width = 5;
+        byte space = 40;
+};
+
+class PipeCollection
+{
+    public:
+        static const byte pipesCount = 5;
+        Pipe pipes[pipesCount];
+
+        PipeCollection()
+        {
+            for (int i = 0; i < pipesCount; i++) {
+                pipes[i].setX(i * 35);
+            }
+        }
+
+        void draw()
+        {
+            for (int i = 0; i < pipesCount; i++) {
+                pipes[i].draw();
+            }
+        }
+
+        void move()
+        {
+            for (int i = 0; i < pipesCount; i++) {
+                pipes[i].move();
+                pipes[i].checkCollisionWithPlayer();
+            }
+        }
+} pipeCollection;
 
 
 /* start ------------------------------------------------------------------- */
@@ -112,7 +230,8 @@ void loop(void)
     } else if (game.stage == 1) {
         // game
         player.checkMovement();
-        player.checkCollision();
+        player.loop();
+        pipeCollection.move();  
     } else if (game.stage == 2) {
         // game over => show score table
         scoreTable.update();
@@ -127,8 +246,9 @@ void loop(void)
                 game.drawMenu();
                 break;
             case 1:
-                game.drawGame();
+                game.draw();
                 player.draw();
+                pipeCollection.draw();
                 break;
             case 2:
                 scoreTable.draw();
